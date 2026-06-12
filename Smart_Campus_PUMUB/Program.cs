6e4ad3492e73f8a1f1009d278e482de7c98b8339
KyawZayarPhyo-22 <22kyawzayarphyo@gmail.com>
2026-06-12 09:19:28 +0630

    "UI Latest Update"

--- Smart_Campus_PUMUB/Program.cs
+++ Smart_Campus_PUMUB/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Smart_Campus_PUMUB.Components;
+using Smart_Campus_PUMUB.Components.Features.Services;
+using Smart_Campus_PUMUB.Database.AppDbContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<SmartCampusDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthenticationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddCascadingAuthenticationState();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
